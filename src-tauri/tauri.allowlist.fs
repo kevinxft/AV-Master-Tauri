494c688936241158f296d4663d5136442b4f3230
MR.Kevin <kevinxft@gmail.com>
2023-09-16 20:00:48 +0800

    增加tauri版本

---
+++ src-tauri/tauri.allowlist.fs
@@ -0,0 +1,18 @@
+{
+  "tauri": {
+    "allowlist": {
+      "fs": {
+        "all": true, // enable all FS APIs
+        "readFile": true,
+        "writeFile": true,
+        "readDir": true,
+        "copyFile": true,
+        "createDir": true,
+        "removeDir": true,
+        "removeFile": true,
+        "renameFile": true,
+        "exists": true
+      }
+    }
+  }
+}
